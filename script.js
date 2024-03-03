2cb1fc5cca06c16ef746c3acb81b489640af44c4
ArpanSankesh <arpansankesh830@gmail.com>
2024-03-03 23:02:21 +0530

    updates

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const Date = document.querySelector('.date')
 const Day = document.querySelector('.day')
 const Month = document.querySelector('.Month')
-const Year = document.querySelector('.Year )
+const Year = document.querySelector('.Year')
+
+const weekDays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
+
+const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
